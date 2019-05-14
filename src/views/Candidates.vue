a75df7605aded5d06be8deef3874d07fcdad3267
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-14 10:04:59 -0300

    Create base of candidates page

---
+++ src/views/Candidates.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>Candidates</h1>
+    <ul>
+      <li v-for="candidate in candidates" :key="candidate.id">
+        {{ candidate.acf.name }}
+        {{ candidate.acf.lastName }} -
+        {{ candidate.acf.phoneNumber }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Candidates',
+  data() {
+    return {
+      candidates: null
+    }
+  },
+  mounted() {
+    axios
+      .get(`${process.env.VUE_APP_ROOT_API}/wp-json/acf/v3/candidates/`)
+      .then(response => (this.candidates = response.data))
+      .catch(err => console.error(`Erro: ${err}`))
+  }
+}
+</script>
